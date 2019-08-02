8b45d673a94d57733bcf46814e7147a5f365b812
Alexander Whipp <alexruns4@gmail.com>
2019-08-02 17:00:49 -0400

    Fixed some facets of the code

--- MT4_Results_To_MonteCarlo.py
+++ MT4_Results_To_MonteCarlo.py
@@ -10,18 +10,25 @@ dd = -0.1
 target = 0.1
 multiplier = 1
 
+# The amount for the initial value
+initial = 1000
+
+# Max number of simulations
+simLimit = 18500
+
 # Read the MT4 Results HTM to memory
 with open(fileName + ".htm", "r") as f:
     lines = f.readlines()
 
-# Convert the HTM file to a legible CSV (assumes a starting balance of $1000)
+# Convert the HTM file to a legible CSV
 with open(fileName + ".csv", "w") as f:
-	f.write("0,1000\n")
-	for line in lines:
-		if toExclude not in line and toFind in line:
-			line = re.sub(r"<.*?>\s?\<.*?\>", ",", line)
-			line = line.split(",")
-			f.write(line[-3] + ',' + line[-2] + '\n')
+    for line in lines:
+        if toExclude not in line and toFind in line:
+            line = re.sub(r"<.*?>\s?\<.*?\>", ",", line)
+            line = line.split(",")
+            if initial == 1000:
+                initial = float(line[-2])
+            f.write(line[-3] + ',' + line[-2] + '\n')
 
 # Creates a Panda dataframe with the results and adds the log returns
 df = pd.read_csv(fileName + '.csv', names=['P/L', 'balance'])
@@ -29,14 +36,18 @@ df['pct_change'] = df.balance.pct_change()
 df['log_ret'] = np.log(df.balance) - np.log(df.balance.shift(1))
 
 # Generates the number of simulations based on the number of trades squared
-simCount = (len(df.index)*len(df.index)) * multiplier
+simCount = int((len(df.index)*len(df.index)) * multiplier)
+if simCount > simLimit:
+    simCount = simLimit
+
+# Runs the simulations
 mc = df['log_ret'].montecarlo(sims=simCount, bust=dd, goal=target)
 
 # Returns percentage statistics on the simulations
 print("===============")
 print(mc.stats)
-print("Profit:", 1000*(1+mc.stats['min']))
-print("Max Drawdown:", 1000*(1+mc.stats['maxdd']))
+print("Profit:", initial*(1+mc.stats['min']))
+print("Max Drawdown:", initial*(1+mc.stats['maxdd']))
 print("Under ", str(dd*100) ,"% drawdown:", mc.stats['bust'] * 100)
 print("Over ", str(target*100) ,"% target:", mc.stats['goal'] * 100)
 print("===============")
